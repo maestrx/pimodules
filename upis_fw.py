ff41cbbf2ca3264294bb9a24d0fc6033c3c6c22a
maestrx <github@safar.info>
2022-04-20 13:44:50 +0200

    poc release 5

--- upis_fw.py
+++ upis_fw.py
@@ -17,15 +17,14 @@
 # - Intel HEX file docs: https://en.wikipedia.org/wiki/Intel_HEX
 # - One line fw update: # i2cget -y 1 0x69 0x38 w && i2cset -y 1 0x6b 0x00 0xff && sleep 2 && ./upis_fw.py ups_pico4_main_testing1.hex && sleep 5 && i2cget -y 1 0x69 0x38 w
 # AUTHOR:       Vit SAFAR <PIco@safar.info>
-# VERSION:      POC release 4
+# VERSION:      POC release 5
 # CREATED:      16.4.2022
 # REVISION:     TBD
 #
 # TODO:         
 # - Script pre-requisities verification & installation (configure boot params, enable I2C&serial, etc). /boot/config.txt: dtparam=i2c_arm=on dtparam=i2c1_baudrate=25000
-# - Script parametrization of: verbosity, serial port, serial baudrate, progress bar type, fw file, etc (accept script arguments)
 # - Switch to bootloader mode in the script instead of the bash command line (can be disabled)
-# - Check that the FW in the file is newer than the one already running (overridable) Is it possible to get the FW version via I2C? Or only via serial?
+# - Check that the FW in the file is newer than the one already running (overridable) 
 # - Manage the pico_i2c.service
 # - Enable RTC
 # - Install pip2
@@ -39,47 +38,104 @@
 #===============================================================================
 
 from __future__ import print_function
-import serial
 import time
 import sys
 import logging
 import re
+import argparse
 from progress.bar import Bar
+import serial
 
-class upsPico:
-    serial_port = None 
-    serial_baudrate = None
-    firmware_filename = None
+
+class picoUPS:
+    args = None
 
     def __init__(self):
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s' )
-        self.firmware_filename = sys.argv[1]
-        self.serial_baudrate = 9600 
-        self.serial_port = '/dev/serial0'
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s' )
+        self.parse_args()
 
-        if self.validate_file():
-            logging.info('Firmware file verification OK.')
-        else:
-            return None
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description='PICO toolkit')
+        parser.add_argument("-v", "--verbose", dest="verbose", help='', action="count", default=0)
 
-        try:
-            self.upis = serial.Serial(port=self.serial_port,baudrate=self.serial_baudrate,timeout=0.001,rtscts=0,xonxoff=True)
-        except:
-            logging.exception('Failed to open serial port')
-            return None
+        group_fwupdate = parser.add_argument_group('Firmware', 'Firmware update')
+        group_fwupdate.add_argument("-f", "--fw-file", dest="fw_file", help='', type=str, action="store", default=None, metavar='firmware_file')
+        group_fwupdate.add_argument("-p", "--serial-port", dest="serial_port", help='', type=str, action="store", default="/dev/serial0", metavar='serial_port_device')
+        group_fwupdate.add_argument("-b", "--baudrate", dest="baud_rate", help='', type=int, action="store", default=9600, metavar='baud_rate')
+        group_fwupdate.add_argument("-j", "--skip-fw-verify", dest="skip_fw_verify", help='', action="store_true")
+        group_fwupdate.add_argument("-J", "--fw-verify-only", dest="fw_verify_only", help='', action="store_true")        
 
-        if self.send_line(":020000040000FA\r"):
-            logging.info('Serial link with UPIS verified')
-        else:
-            logging.error('Failed to confirm serial communication with UPIS')
-            return None
+        group_setup = parser.add_argument_group('Setup', 'Setup of the RPi for PICO UPS')
+        group_setup.add_argument('-s', '--setup', dest="skip_fw_verify", help='', action="store_true")
 
-        if self.send_file():
-            logging.info('Firmware update completed')
+       
+        '''
+import argparse
+
+my_parser = argparse.ArgumentParser()
+my_parser.version = '1.0'
+my_parser.add_argument('-a', action='store')
+my_parser.add_argument('-b', action='store_const', const=42)
+my_parser.add_argument('-c', action='store_true')
+my_parser.add_argument('-d', action='store_false')
+my_parser.add_argument('-e', action='append')
+my_parser.add_argument('-f', action='append_const', const=42)
+my_parser.add_argument('-g', action='count')
+my_parser.add_argument('-i', action='help')
+my_parser.add_argument('-j', action='version')
+
+args = my_parser.parse_args()
+
+print(vars(args))
+
+, required=True
+
+        '''
+        
+        self.args = parser.parse_args()
+        print (self.args)
+
+        # NOTSET<DEBUG<INFO<WARNING<ERROR<CRITICAL
+        if self.args.verbose > 1:
+            logging.getLogger().setLevel(logging.DEBUG)
+            logging.debug('Logging set to DEBUG')
+        elif self.args.verbose > 0:
+            logging.getLogger().setLevel(logging.INFO)
+            logging.info('Logging set to INFO')
+        elif self.args.verbose < 1:
+            logging.getLogger().setLevel(logging.WARNING)
+
+        # FW upload action
+        if self.args.fw_file: 
+            if self.args.skip_fw_verify:
+                logging.warning('Skipping firmware verification')
+            else:
+                if self.validate_file():
+                    logging.warning('Firmware file verification OK')
+                else:
+                    return None
+            if self.args.fw_verify_only:
+                return None
+
+            try:
+                self.upis = serial.Serial(port=self.args.serial_port, baudrate=self.args.baud_rate, timeout=0.001, rtscts=0, xonxoff=True)
+            except:
+                logging.exception('Failed to open serial port')
+                return None
+
+            if self.send_line(":020000040000FA\r"):
+                logging.warning('Serial link with UPIS verified')
+            else:
+                logging.error('Failed to establish serial communication with PICO UPS')
+                return None
+
+            if self.send_file():
+                logging.warning('Firmware update completed')
+
 
     def validate_file(self):
         eof = False
-        f = open(self.firmware_filename)
+        f = open(self.args.fw_file)
         lnum = 0
         for line in f:
             line=line.strip()
@@ -98,14 +154,14 @@ class upsPico:
 
     def send_file(self):
         try:
-            fw_lines_total = sum(1 for line in open(self.firmware_filename) if line.startswith(':'))
+            fw_lines_total = sum(1 for line in open(self.args.fw_file) if line.startswith(':'))
             bar = Bar('Uploading firmware', max=fw_lines_total, suffix='%(percent).1f%% - %(eta)ds')
-            f = open(self.firmware_filename)
+            f = open(self.args.fw_file)
             lnum = 0
             for line in f:
                 lnum += 1
                 if not self.send_line(line.strip()+"\r"):
-                    logging.error('failed to ack the line {0}'.format(lnum))
+                    logging.error('Failed to ack the line {0}'.format(lnum))
                     break
                 bar.next()
                 if line[7:9]=='01':  # this is the last FW file line
@@ -153,7 +209,7 @@ class upsPico:
             logging.exception('Failed to recieve data ACK from UPIS')
             return False
 
-upsPico()
+picoUPS()
 
 
 
